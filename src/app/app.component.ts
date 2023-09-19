654389e8bcce2220a9e6a959be996097ec1609e7
LENOVO <LENOVO@DESKTOP-RF2OJ7S>
2023-09-19 20:30:45 +0530

    End of Sec #3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Dating App';
+  users:any;
+
+  constructor(
+    private http:HttpClient,
+    ){}
+    ngOnInit():void{
+      this.http.get('https://localhost:5001/api/users').subscribe({
+        next:resp=> this.users= resp,
+        error:err=>console.log(err),
+        complete:()=> console.log('Users Reaquest has completed')
+      })
+    }
 }
